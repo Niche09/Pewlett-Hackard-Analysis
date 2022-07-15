32fdb7165ae3ea60c4281d82fecad2dea8c2aea6
Niche09 <106127571+Niche09@users.noreply.github.com>
2022-07-14 22:54:24 -0400

    Add files via upload

---
+++ Employee_Database_Challenge_Deliverable_2.sql
@@ -0,0 +1,104 @@
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE dept_emp(
+emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL
+);
+
+CREATE TABLE managers (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+);
+
+
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL, 
+	hire_date DATE NOT NULL
+);
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+);
+
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+);
+
+SELECT * FROM  departmentemployee;
+
+SELECT emp_no, first_name, last_name
+FROM employees;
+
+SELECT title, from_date, to_date 
+FROM titles; 
+
+SELECT *  FROM retirement_titles;
+
+SELECT employees.emp_no, employees.first_name, employees.last_name,
+titles.title, titles.from_date, titles.to_date
+INTO retirement_titles
+FROM employees, titles
+WHERE  employees.emp_no =titles.emp_no
+AND (birth_date BETWEEN '1952-01-01' AND '1955-01-01');
+
+SELECT * FROM retirement_titles
+ORDER BY emp_no ASC;
+
+SELECT emp_no, first_name, last_name, title
+FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no,
+retirement_titles.first_name,
+retirement_titles.last_name,
+retirement_titles.title
+INTO unique_titles
+FROM retirement_titles 
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT COUNT(unique_titles.emp_no), unique_titles.title
+INTO retiring_titles
+FROM unique_titles 
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM retiring_titles;
+
+--Deliverable 2
+
+SELECT DISTINCT ON(employees.emp_no)employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.birth_date,
+	dept_emp.from_date,
+	dept_emp.to_date,
+	titles.title
+INTO mentorship_table
+FROM employees 
+	INNER JOIN dept_emp 
+		ON (employees.emp_no = dept_emp.emp_no)
+			INNER JOIN titles 
+				ON (employees.emp_no = titles.emp_no)
+WHERE (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (dept_emp.to_date = '9999-01-01')
+ORDER BY emp_no;
